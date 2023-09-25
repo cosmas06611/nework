54e7d96eb915f247766184a2e9a4d1f2ed0060ed
khosi <cosmasndulue00611@gmail.com>
2023-09-25 19:20:39 +0100

    adding mywork

---
+++ src/thisProjectApp/App.js
@@ -0,0 +1,17 @@
+import Cards from './Card';
+import Navigation from './Navbar';
+import Heading from './Heading'
+
+    function App() {
+    return (
+        <div className = "App">
+            <Heading />
+        <Navigation />
+        <Cards />
+        <Cards />
+        <Cards />
+        </div>
+    );
+    }
+    export default App
+
---
+++ src/thisProjectApp/Heading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+    function Heading(){
+    return(
+        <div>
+    <ul className="dropdown-menu">
+    <li><h6 className="dropdown-header">Dropdown header</h6></li>
+    <li><a className="dropdown-item" href="Home">Home</a></li>
+    <li><a className="dropdown-item" href="Action">Another action</a></li>
+    </ul>
+        </div>
+
+        )
+    }
+    export default Heading
